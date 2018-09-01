0227ab2da85cdd76e42d96ca7cd83cb6d8084d61
Hrushikesh <hmulavekar@gmail.com>
2018-09-01 11:10:39 +0530

    updates

---
+++ app/src/main/java/com/example/hrushi/withoutfirebas/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.hrushi.withoutfirebas;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.AsyncTask;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+    RelativeLayout constraintLayout;
+    AnimationDrawable animationDrawable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        constraintLayout = findViewById(R.id.myback);
+        animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(4500);
+        animationDrawable.setExitFadeDuration(4500);
+        animationDrawable.start();
+
+
+}}
